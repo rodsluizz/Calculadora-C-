68a152a47a298d5e52218bd9c932457c5afe4cb0
RODRIGO <rodrigoluizcferreira@gmail.com>
2021-05-20 21:05:29 -0300

    Utilizando métodos

--- Program.cs
+++ Program.cs
@@ -6,22 +6,47 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Console.Clear();
+            Menu();
+        }
+        static void Menu(){
             Console.WriteLine("Digite um valor: ");
             float v1 = float.Parse(Console.ReadLine());
             Console.WriteLine("Digite outro valor: ");
             float v2 = float.Parse(Console.ReadLine());
-            
+            Console.Clear();
+
             Console.WriteLine("1. SOMAR | 2. SUBTRAIR | 3.MULTIPLICAR | 4.DIVIDIR");
             int operador = int.Parse(Console.ReadLine());
+            Console.Clear();
 
             switch(operador){
-                case 1: Console.WriteLine(v1 + v2); break;
-                case 2: Console.WriteLine(v1 - v2); break;
-                case 3: Console.WriteLine(v1 * v2); break;
-                default: Console.WriteLine(v1 / v2); break;
+                case 1: Somar(v1, v2); break;
+                case 2: Subtrair(v1, v2); break;
+                case 3: Multiplicar(v1, v2); break;
+                case 4: Dividir(v1, v2); break;
+                default: Menu(); break;
             }
-
         }
+        static void Somar(float n1, float n2){
+            float resultado = n1 + n2;
+            Console.WriteLine(resultado);
+            Menu();
+        }
+        static void Subtrair(float n1, float n2){
+            float resultado = n1 - n2;
+            Console.WriteLine(resultado);
+            Menu();
+        }
+        static void Multiplicar(float n1, float n2){
+            float resultado = n1 * n2;
+            Console.WriteLine(resultado);
+            Menu();
+        }
+        static void Dividir(float n1, float n2){
+            float resultado = n1 * n2;
+            Console.WriteLine(resultado);
+            Menu();
+        }
+
     }
 }
